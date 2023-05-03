7948840890cbe9733f7fb6c98e7db6d6387934dd
Sma-source <rabhi.smain@gmail.com>
2023-05-03 17:42:57 +0200

    update Home.jsx: make imports

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,12 @@
-import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
 
 function Home() {
   return <div>Home</div>;
