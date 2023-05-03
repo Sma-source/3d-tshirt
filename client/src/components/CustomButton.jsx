598457d78d2e9a3f74162ad6e95536fa2de1a847
Sma-source <rabhi.smain@gmail.com>
2023-05-03 19:34:48 +0200

    update CustomButton.jsx: add function to generate styles

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 
-const CustomButton = ({ type, title, handleCick, customStyles }) => {
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+
+const CustomButton = ({ type, title, handleClick, customStyles }) => {
+  const snap = useSnapshot(state);
+  const generateStyle = (type) => {
+    if (type === "filled") {
+      return {
+        backgroundColor: snap.color,
+        color: "#ffffff",
+      };
+    } else if (type === "outline") {
+      return {
+        borderWidth: "1px",
+        borderColor: snap.color,
+        color: snap.color,
+      };
+    }
+  };
   return (
     <button
       className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
-      onClick={handleCick}
+      onClick={handleClick}
+      style={generateStyle(type)}
     >
       {title}
     </button>
