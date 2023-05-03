d2f976299c4018367206f7906181c9b97af165b7
Sma-source <rabhi.smain@gmail.com>
2023-05-03 19:27:31 +0200

    update CustomButton.jsx: pass props

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const CustomButton = ({ type, title, handleCick, customStyles }) => {
-  return <bu>CustomButton</bu>;
+  return (
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      onClick={handleCick}
+    >
+      {title}
+    </button>
+  );
 };
 
 export default CustomButton;
