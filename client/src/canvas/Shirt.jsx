1f1114b844d31986bccf4d07faf13e61e3988e86
Sma-source <rabhi.smain@gmail.com>
2023-05-04 19:05:27 +0200

    update canvas/Shirt.jsx: make all imports

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { useFrame } from "@react-three/fiber";
+import { Decal, useGLTF, useTexture } from "@react-three/drei";
+
+import state from "../store";
 
 const Shirt = () => {
+  const snap = useSnapshot(state);
   return <div>Shirt</div>;
 };
 
