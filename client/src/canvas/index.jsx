bf2cd9d3b08e259e9ee422f98854a596a00d1247
Sma-source <rabhi.smain@gmail.com>
2023-05-04 18:58:47 +0200

    update canvas/index.js: render Shirt component

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -10,6 +10,12 @@ const CanvasModel = () => {
     <Canvas>
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
+      {/* <CameraRig>
+        <Backdrop /> */}
+      <Center>
+        <Shirt />
+      </Center>
+      {/* </CameraRig> */}
     </Canvas>
   );
 };
