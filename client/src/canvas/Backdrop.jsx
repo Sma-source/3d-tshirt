8be08e7e9d4948858f3b51991612a9f345de25b9
Sma-source <rabhi.smain@gmail.com>
2023-05-05 19:13:00 +0200

    update Backdrop.jsx: make all imports

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
+import { easing } from "maath";
+import { useFrame } from "@react-three/fiber";
+import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 
 const Backdrop = () => {
+  const shadows = useRef();
   return <div>Backdrop</div>;
 };
 
