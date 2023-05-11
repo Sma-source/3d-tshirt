f228d1eae0a6f8096a19a260a212dd568536f842
Sma-source <rabhi.smain@gmail.com>
2023-05-11 11:58:13 +0200

    update AIPicker.jsx: create textarea

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -3,7 +3,17 @@ import React from "react";
 import CustomButton from "./CustomButton";
 
 const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
-  return <div>AIPicker</div>;
+  return (
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Ask AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aipicker-textarea"
+      />
+    </div>
+  );
 };
 
 export default AIPicker;
